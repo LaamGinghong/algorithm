774ff5743da5e600e328e479f846b403e6125dbf
LaamGinghong <lanjinghang@banggood.com>
2018-10-29 16:01:40 +0800

    完成求最小公倍数

--- leastCommonMultiple.mjs
+++ leastCommonMultiple.mjs
@@ -4,9 +4,13 @@ class LeastCommonMultiple {
     constructor() {
     }
 
-    static recursion(numberA, numberB) {
+    /*
+     * param {numberA} number
+     * param {numberB} number
+     * return number
+     */
+    static recursion(numberA, numberB) { //两数相乘的绝对值除以他们的最大公约数即为最大公倍数
         return (numberA === 0 || numberB === 0) ? 0 : Math.abs(numberA * numberB) / EuclideanAlgorithm.recursion(numberA, numberB);
     }
 }
 
-console.log(LeastCommonMultiple.recursion(10, 20));
