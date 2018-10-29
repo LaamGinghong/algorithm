0158e81d565bad14a17ad3b3737ece51084c2ddb
LaamGinghong <lanjinghang@banggood.com>
2018-10-29 14:09:40 +0800

    完成素数检查

--- primeNumberCheck/primeNumberCheck.js
+++ primeNumberCheck/primeNumberCheck.js
@@ -1,6 +1,29 @@
 class primeNumberCheck {
 
-    constructor(number) {
-        this.number = number;
+    constructor() {
+    }
+
+    static trialDivision(number) {
+        if (number % 1 !== 0) { //去除非整数
+            return false;
+        }
+        if (number <= 1) { //去除0,1
+            return false;
+        }
+        if (number <= 3) { //2,3都是质数
+            return true;
+        }
+        if (number % 2 === 0) { //2的倍数都不是质数
+            return false;
+        }
+        const dividerLimit = Math.sqrt(number); //如果没有除以n的平方根的除数那么也没有更高的除数
+        for (let divider = 3; divider <= dividerLimit; divider += 2) {
+            if (number % divider === 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
+
+console.log(primeNumberCheck.trialDivision(3));
