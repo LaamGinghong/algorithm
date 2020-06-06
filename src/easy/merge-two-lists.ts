6f3871e1ded8bc48b38ec3e4df477d0a5ada2554
LaamGinghong <351390560@qq.com>
2020-06-06 21:33:40 +0800

    merge-two-lists.ts

---
+++ src/easy/merge-two-lists.ts
@@ -0,0 +1,20 @@
+interface ListNode {
+    value?: ListNode
+    next?: ListNode
+}
+
+function mergeTwoLists(l1: ListNode, l2: ListNode) {
+    if (l1 === null || l1 === undefined) {
+        return l2
+    } else if (l2 === null || l2 === undefined) {
+        return l1
+    } else if (l1.value < l2.value) {
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
+    }
+}
+
+console.log(mergeTwoLists({}, {}))
