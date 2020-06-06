2301a05ab207a53af4620b31ddd7bcf55c43e4a7
LaamGinghong <351390560@qq.com>
2020-06-07 01:33:37 +0800

    climb-stairs.ts

--- src/easy/climb-stairs.ts
+++ src/easy/climb-stairs.ts
@@ -13,9 +13,9 @@ const climbStairs1 = (function () {
 
 function climbStairs2(n: number) {
     const dp = []
-    dp[0] = 1
     dp[1] = 1
-    for (let i = 2; i <= n; i++) {
+    dp[2] = 2
+    for (let i = 3; i <= n; i++) {
         dp[i] = dp[i - 1] + dp[i - 2]
     }
     return dp[n]
