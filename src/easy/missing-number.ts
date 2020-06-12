ec1024d4d7ea6a7f3113f8c48a5605b0adb1277d
LaamGinghong <351390560@qq.com>
2020-06-13 00:49:31 +0800

    missing-number.ts

---
+++ src/easy/missing-number.ts
@@ -0,0 +1,9 @@
+function missingNumber(nums: number[]) {
+    nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== i) {
+            return i
+        }
+    }
+    return nums.length
+}
