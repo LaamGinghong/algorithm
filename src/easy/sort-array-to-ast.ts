487236e2e46073e3b12e3a3a8c37bcaffb1993e7
LaamGinghong <351390560@qq.com>
2020-06-15 22:52:14 +0800

    sort-array-to-ast.ts

---
+++ src/easy/sort-array-to-ast.ts
@@ -0,0 +1,25 @@
+import { TreeNode as TreeNodeOptions } from '../tree-node'
+
+class TreeNode implements TreeNodeOptions {
+  value: number
+  left: TreeNode
+  right: TreeNode
+
+  constructor(node: number) {
+    this.value = node
+  }
+}
+
+function sortArrayToAST(nums: number[]) {
+  function helper(left: number, right: number) {
+    if (left > right) return null
+
+    const middle = Math.floor((left + right) / 2)
+    const node = new TreeNode(nums[middle])
+    node.left = helper(left, middle - 1)
+    node.right = helper(middle + 1, right)
+    return node
+  }
+
+  return helper(0, nums.length - 1)
+}
