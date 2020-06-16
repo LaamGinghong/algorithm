9f8df0eb4f41fbe27d261b9c2f4bbeee73da9bd3
LaamGinghong <351390560@qq.com>
2020-06-16 22:59:14 +0800

    max-depth.ts

--- src/easy/max-depth.ts
+++ src/easy/max-depth.ts
@@ -1,8 +1,49 @@
-import {TreeNode} from "../tree-node";
+import { TreeNode } from '../tree-node'
 
-function maxDepth(root: TreeNode) {
-    if (!root) {
-        return 0
-    }
-    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+// 二叉树
+function maxDepth1(root: TreeNode) {
+  if (!root) {
+    return 0
+  }
+  return Math.max(maxDepth1(root.left), maxDepth1(root.right)) + 1
 }
+
+interface NTreeNode {
+  value: number
+  children: NTreeNode[]
+}
+
+// N叉数
+function maxDepth2(root: NTreeNode) {
+  let maxDepth = Number.MIN_SAFE_INTEGER
+  function helper(node: NTreeNode, depth: number) {
+    if (!node) return
+    if (!node.children.length) {
+      maxDepth = Math.max(depth, maxDepth)
+      return
+    }
+    node.children.forEach((child) => {
+      helper(child, depth + 1)
+    })
+  }
+
+  helper(root, maxDepth)
+  return maxDepth
+}
+
+console.log(
+  maxDepth2({
+    value: 1,
+    children: [
+      {
+        value: 3,
+        children: [
+          { value: 5, children: [] },
+          { value: 6, children: [] },
+        ],
+      },
+      { value: 2, children: [] },
+      { value: 4, children: [] },
+    ],
+  }),
+)
