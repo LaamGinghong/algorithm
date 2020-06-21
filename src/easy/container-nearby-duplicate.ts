91d9ad12f91cfe74583bc2553c3b1033432c0c58
LaamGinghong <351390560@qq.com>
2020-06-21 23:34:00 +0800

    container-nearby-duplicate.ts

---
+++ src/easy/container-nearby-duplicate.ts
@@ -0,0 +1,10 @@
+function containerNearbyDuplicate(nums: number[], k: number) {
+  const set = new Set<number>()
+
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) return true
+    set.add(nums[i])
+    if (set.size > k) set.delete(nums[i - k])
+  }
+  return false
+}
