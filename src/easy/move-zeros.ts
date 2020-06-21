724c6bb094f00d8d8e3fa6a93bd0f44b7cfcf74a
LaamGinghong <351390560@qq.com>
2020-06-21 23:05:20 +0800

    move-zeros.ts

--- src/easy/move-zeros.ts
+++ src/easy/move-zeros.ts
@@ -1,18 +1,28 @@
-function moveZeros(nums: number[]) {
-    let index = 0
-    let {length} = nums
-    const zeros = []
-    while (index < length) {
-        const num = nums[index]
-        if (num === 0) {
-            nums.splice(index, 1)
-            length--
-            zeros.push(0)
-        } else {
-            index++
-        }
+function moveZeros1(nums: number[]) {
+  let index = 0
+  let { length } = nums
+  const zeros = []
+  while (index < length) {
+    const num = nums[index]
+    if (num === 0) {
+      nums.splice(index, 1)
+      length--
+      zeros.push(0)
+    } else {
+      index++
     }
-    nums.push(...zeros)
+  }
+  nums.push(...zeros)
 }
 
-moveZeros([0, 1, 0, 3, 12])
+function moveZeros2(nums: number[]) {
+  let j = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      const temp = nums[j]
+      nums[j] = nums[i]
+      nums[i] = temp
+      j++
+    }
+  }
+}
