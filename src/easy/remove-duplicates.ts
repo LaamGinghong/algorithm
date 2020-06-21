14e795d099538a1ded590cfdf95ebeb348f64e12
LaamGinghong <351390560@qq.com>
2020-06-21 22:53:25 +0800

    remove-duplicates.ts

--- src/easy/remove-duplicates.ts
+++ src/easy/remove-duplicates.ts
@@ -1,15 +1,13 @@
 function removeDuplicates(nums: number[]): number {
-    const length = new Set(nums).size
-    let current = nums[0]
-    for (let i = 1; i < length;) {
-        const num = nums[i]
-        if (num === current) {
-            nums.splice(i, 1)
-        } else {
-            current = nums[i++]
-        }
+  const length = new Set(nums).size
+  let current = nums[0]
+  for (let i = 1; i < length; ) {
+    const num = nums[i]
+    if (num === current) {
+      nums.splice(i, 1)
+    } else {
+      current = nums[i++]
     }
-    return length
+  }
+  return length
 }
-
-removeDuplicates([1, 1, 2])
