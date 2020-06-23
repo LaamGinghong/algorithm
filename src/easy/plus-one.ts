21bf818d239e767ae411586f62c8913f9f0bd02f
蓝京杭 <lanjinghang@guanmai.cn>
2020-06-24 01:08:02 +0800

    plus-one.ts

--- src/easy/plus-one.ts
+++ src/easy/plus-one.ts
@@ -1,14 +1,12 @@
 function plusOne(digits: number[]) {
-    const {length} = digits
-    for (let i = length - 1; i >= 0; i--) {
-        digits[i]++
-        digits[i] %= 10
-        if ((digits[i] !== 0)) {
-            return digits
-        }
+  const { length } = digits
+  for (let i = length - 1; i >= 0; i--) {
+    digits[i]++
+    digits[i] %= 10
+    if (digits[i] !== 0) {
+      return digits
     }
-    digits = (new Array(length + 1)).fill(0).map((_, index) => index ? 0 : 1)
-    return digits
+  }
+  digits = new Array(length + 1).fill(0).map((_, index) => (index ? 0 : 1))
+  return digits
 }
-
-console.log(plusOne([1, 2, 3]))
