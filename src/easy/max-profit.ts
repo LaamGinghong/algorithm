a2c7422ee73b914ddfeed1cb3dd873a7723b75f1
蓝京杭 <lanjinghang@guanmai.cn>
2020-06-29 00:03:53 +0800

    max-profit.ts

--- src/easy/max-profit.ts
+++ src/easy/max-profit.ts
@@ -1,14 +1,27 @@
-function maxProfit(prices: number[]) {
-    let min = prices[0]
-    let max = 0
-    for (let i = 1; i < prices.length; i++) {
-        if (prices[i] < min) {
-            min = prices[i]
-        } else {
-            max = Math.max(max, prices[i] - min)
-        }
+function maxProfit1(prices: number[]) {
+  let min = prices[0]
+  let max = 0
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < min) {
+      min = prices[i]
+    } else {
+      max = Math.max(max, prices[i] - min)
     }
-    return max
+  }
+  return max
 }
 
-console.log(maxProfit([7, 1, 5, 3, 6, 4]))
+function maxProfit2(prices: number[]) {
+  let index = 0
+  let valley = prices[0]
+  let peek = prices[0]
+  let maxProfit = 0
+  while (index < prices.length - 1) {
+    while (index < prices.length - 1 && prices[index] <= prices[index + 1]) index++
+    peek = prices[index]
+    while (index < prices.length - 1 && prices[index] >= prices[index + 1]) index++
+    valley = prices[index]
+    maxProfit += peek - valley
+  }
+  return maxProfit
+}
