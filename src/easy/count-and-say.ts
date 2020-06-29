6b1d55767a2fa6e139bcab480ca48d9c8bff14d9
LaamGinghong <351390560@qq.com>
2020-06-29 23:36:40 +0800

    count-and-say.ts

--- src/easy/count-and-say.ts
+++ src/easy/count-and-say.ts
@@ -1,21 +1,20 @@
 function countAndSay(n: number) {
-    if (n === 1) {
-        return '1'
+  if (n === 1) {
+    return '1'
+  }
+  const zh = (a: string) => {
+    let m = 1
+    let res = ''
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] === a[i + 1]) {
+        m++
+      } else {
+        res = res + m + a[i]
+        m = 1
+      }
     }
-    const zh = (a: string) => {
-        let m = 1, res = ''
-        for (let i = 0; i < a.length; i++) {
-            if (a[i] === a[i + 1]) {
-                m++
-            } else {
-                res = res + m + a[i]
-                m = 1
-            }
-        }
-        n--
-        return n === 1 ? res : zh(res)
-    }
-    return zh('1')
+    n--
+    return n === 1 ? res : zh(res)
+  }
+  return zh('1')
 }
-
-console.log(countAndSay(3))
