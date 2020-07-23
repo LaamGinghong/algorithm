8aaeb49f39714bfc45512e1abd677ddbf50ee4cb
蓝京杭 <lanjinghang@guanmai.cn>
2020-07-23 23:25:32 +0800

    search.ts

---
+++ src/middle/search.ts
@@ -0,0 +1,28 @@
+function search(nums: number[], target: number) {
+  const n = nums.length
+  if (!n) return -1
+  if (n === 1) return nums[0] === target ? 0 : -1
+
+  let l = 0
+  let r = n - 1
+
+  while (l <= r) {
+    const mid = parseInt(`${(l + r) / 2}`)
+    if (nums[mid] === target) return mid
+
+    if (nums[0] <= nums[mid]) {
+      if (nums[0] <= target && target < nums[mid]) {
+        r = mid - 1
+      } else {
+        l = mid + 1
+      }
+    } else {
+      if (nums[mid] <= target && target <= nums[n - 1]) {
+        l = mid + 1
+      } else {
+        r = mid - 1
+      }
+    }
+  }
+  return -1
+}
