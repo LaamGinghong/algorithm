63bc8b7f723e24befee4117cdc350fb53ad34881
LaamGinghong <351390560@qq.com>
2020-08-01 10:58:06 +0800

    massage.ts

--- src/easy/massage.ts
+++ src/easy/massage.ts
@@ -1,8 +1,14 @@
 function massage(nums: number[]) {
-  const dp: number[] = [0, 1]
-  for (let i = 2; i < nums.length + 2; i++) {
-    dp[i] = Math.max(dp[i - 1], nums[i - 2] + dp[i - 2])
+  const n = nums.length
+  if (!n) return 0
+  if (n === 1) return nums[0]
+  const dp = Array(n + 1).fill(0)
+  dp[0] = nums[0]
+  dp[1] = Math.max(nums[0], nums[1])
+
+  for (let i = 2; i < n; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
   }
 
-  return dp[nums.length + 1]
+  return dp[n - 1]
 }
