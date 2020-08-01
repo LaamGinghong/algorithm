096e253b6a58e7a3902b12118937d861e1e1bc54
LaamGinghong <351390560@qq.com>
2020-08-01 22:51:42 +0800

    num-ways.ts

--- src/easy/num-ways.ts
+++ src/easy/num-ways.ts
@@ -1,10 +1,16 @@
+/*
+  动态规划
+  如果当前 i 和 i - 1 个颜色相同，则 i 和 i - 2 个颜色不同，
+  如果当前 i 和 i - 1 个颜色不同
+ */
 function numWays(n: number, k: number) {
-    const arr = []
-    arr[0] = 0
-    arr[1] = k
-    arr[2] = k * k
-    for (let i = 3; i < n + 1; i++) {
-        arr[i] = (k - 1) * arr[i - 1] + (k - 1) * arr[i - 2]
-    }
-    return arr[n]
+  if (n === 0 || k === 0) return 0
+  if (n === 1) return k
+  if (n === 2) return k * k
+
+  const dp = [k, k * k]
+  for (let i = 2; i < n; i++) {
+    dp[i] = dp[i - 1] * (k - 1) + dp[i - 2] * (k - 1)
+  }
+  return dp[n - 1]
 }
