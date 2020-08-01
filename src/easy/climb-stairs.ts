0fbac685b747e65872ed816ea197fc25242a1824
LaamGinghong <351390560@qq.com>
2020-08-01 21:30:25 +0800

    climb-stairs.ts

--- src/easy/climb-stairs.ts
+++ src/easy/climb-stairs.ts
@@ -1,22 +1,22 @@
 const climbStairs1 = (function () {
-    const cache: { [key: number]: number } = {
-        1: 1,
-        2: 2
+  const cache: { [key: number]: number } = {
+    1: 1,
+    2: 2,
+  }
+  return function (n: number) {
+    if (typeof cache[n] !== 'number') {
+      cache[n] = climbStairs1(n - 1) + climbStairs1(n - 2)
     }
-    return function (n: number) {
-        if (typeof cache[n] !== "number") {
-            cache[n] = climbStairs1(n - 1) + climbStairs1(n - 2)
-        }
-        return cache[n]
-    }
+    return cache[n]
+  }
 })()
 
 function climbStairs2(n: number) {
-    const dp = []
-    dp[1] = 1
-    dp[2] = 2
-    for (let i = 3; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2]
-    }
-    return dp[n]
+  const dp = []
+  dp[0] = 1
+  dp[1] = 2
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2]
+  }
+  return dp[n]
 }
