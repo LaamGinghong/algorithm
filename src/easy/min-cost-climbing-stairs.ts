fcfb4769cf5adf8ce3cc9582020384f854b108c7
LaamGinghong <351390560@qq.com>
2020-08-01 21:40:25 +0800

    min-cost-climbing-stairs.ts

--- src/easy/min-cost-climbing-stairs.ts
+++ src/easy/min-cost-climbing-stairs.ts
@@ -3,8 +3,10 @@
   动态规划
  */
 function minCostClimbingStairs(cost: number[]) {
-  const dp: number[] = [cost[0], cost[1]]
   const n = cost.length
+  if (!n) return 0
+  if (n === 1) return cost[0]
+  const dp: number[] = [cost[0], cost[1]]
 
   for (let i = 2; i < n; i++) {
     dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i]
