58e697ac7dcf52a097d18a362309f13600d3e006
LaamGinghong <351390560@qq.com>
2020-08-01 22:23:19 +0800

    rob.ts

--- src/easy/rob.ts
+++ src/easy/rob.ts
@@ -1,11 +1,18 @@
+/*
+    动态规划
+    首先排除长度为 0 和 1
+    dp 表示在第 i 个房子时候的最优解，是偷着房子还是不偷
+    公式为 dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1])
+ */
 function rob(nums: number[]) {
-    const {length} = nums
-    if (!length) return 0
-    const dp = new Array(length + 1)
-    dp[0] = 0
-    dp[1] = nums[0]
-    for (let i = 2; i <= length; i++) {
-        dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1])
-    }
-    return dp[length]
+  const n = nums.length
+  if (!n) return 0
+  if (n === 1) return nums[0]
+
+  const dp = [nums[0], Math.max(nums[0], nums[1])]
+  for (let i = 2; i < n; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
+  }
+
+  return dp[n - 1]
 }
